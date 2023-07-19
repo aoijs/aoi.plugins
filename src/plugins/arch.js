ce0aafdbfa3cb84bb3ebed75e84ed7d43a745468
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-07-19 04:12:57 +0300

    metadata update

--- src/plugins/arch.js
+++ src/plugins/arch.js
@@ -1,6 +1,10 @@
 module.exports = {
     name: '$arch',
     type: 'djs',
+    author: "dodoGames",
+    version: "6.4.0",
+    description: "Returns the current system architecture the host/OS is using.",
+    example: "Will return x64, arm, arm64, ppc, etc depending on the current system architecture used",
     code: async d => {
         const data = d.util.aoiFunc(d);
         data.result = process.arch;
--- src/plugins/djsversion.js
+++ src/plugins/djsversion.js
@@ -1,6 +1,10 @@
 module.exports = {
     name: '$djsversion',
     type: 'djs',
+    author: "dodoGames",
+    version: "6.4.0",
+    description: "Returns the current discord.js version the bot is using",
+    example: "Will return 14.11.0 as a example",
     code: async d => {
         const data = d.util.aoiFunc(d);
         
