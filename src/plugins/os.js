42af96d3425c7b46cf8677a50f0afc4539a537cb
Leref <12578483+Leref@users.noreply.github.com>
2023-07-18 20:58:46 -0400

    metadata.

--- src/plugins/os.js
+++ src/plugins/os.js
@@ -1,6 +1,10 @@
 module.exports = {
     name: '$os',
     type: 'djs',
+    author: "default",
+    version: "6.4.0",
+    description: "Returns the OS of the system",
+    example: "The OS of the system is $os",
     code: async d => {
         const data = d.util.aoiFunc(d);
         const os = require('os');
