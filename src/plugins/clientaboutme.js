e05ce71ad473f1af29529cc76ec3a6d058edc523
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-07-19 04:20:39 +0300

    minor update

--- src/plugins/clientaboutme.js
+++ src/plugins/clientaboutme.js
@@ -4,7 +4,7 @@ module.exports = {
     author: "dodoGames",
     version: "6.4.0",
     description: "Returns the bot about me by returning the application description from developer portal",
-    example: "if the application description is test then it will return test upon running the function",
+    example: "If the application description is test then it will return test upon running the function",
     code: async d => {
         const data = d.util.aoiFunc(d);
         data.result = d.client.application.description;
